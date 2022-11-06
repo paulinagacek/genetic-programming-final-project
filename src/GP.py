a836338aebe490c5a17d8b44474578c0fd715e82
paulinagacek <paulina.gacek.pl@gmail.com>
2022-11-06 13:41:03 +0100

    Printing logs improved

--- src/GP.py
+++ src/GP.py
@@ -1,5 +1,6 @@
 from Node import *
 
+
 class GP:
     def __init__(self) -> None:
         self.max_depth = 2
@@ -7,12 +8,17 @@ class GP:
         self.population = []  # List[Node]
         self.fitness = []  # List[float]
         self.tournament_size = 2
-        self.mutation_rate = 0.1
+        self.mutation_rate = 0.5
         self.crossover_rate = 0.5
-        self.max_generations = 2
+        self.nr_of_generations = 2
 
     def compute_fitness(self, individual: Node) -> float:
         return -random.randint(0, 2137)
+
+    def create_random_population(self):
+        for idx in range(self.population_size):
+            self.population.append(self.create_random_individual())
+            self.fitness.append(self.compute_fitness(self.population[idx]))
 
     def create_random_individual(self) -> Node:
         root, level = Node(NodeType.PROGRAM, [], None), 0
@@ -31,16 +37,10 @@ class GP:
                     queue.append((level+1, node.children[idx]))
         return root
 
-    def create_random_population(self):
-        for idx in range(self.population_size):
-            self.population.append(self.create_random_individual())
-            self.fitness.append(self.compute_fitness(self.population[idx]))
-
-    """
-        Return idx of the best individual
-    """
-
     def perform_tournament(self) -> int:
+        """
+        Returns idx of the best individual
+        """
         fbest = -1.0e34
         best = random.randint(0, self.population_size-1)
         for idx in range(self.tournament_size):
@@ -50,11 +50,10 @@ class GP:
                 best = competitor
         return best
 
-    """
-        Return idx of the worst individual
-    """
-
     def perform_negative_tournament(self) -> int:
+        """
+        Returns idx of the worst individual
+        """
         fworst = 1.0e34
         worst = random.randint(0, self.population_size-1)
         for idx in range(self.tournament_size):
@@ -71,28 +70,34 @@ class GP:
         # implementacja
         return parent1
 
-    """
-        Mutates provided node with other randomly chosen.
-        Not all nodes have alternatives for mutations and such nodes cannot
-        be mutate with point mutation.
-    """
-
     @staticmethod
     def perform_point_mutation(parent: Node) -> Node:
+        """
+        Mutates provided node with other randomly chosen.
+        Not all nodes have alternatives for mutations and such nodes cannot
+        be mutated using point mutation.
+        """
         possibilities = Node.get_possible_point_mutations(parent.type)
-        if len(possibilities) == 0:
+        if len(possibilities) == 0:  # node cannot be mutated
             return parent
         else:
             idx = random.randint(0, len(possibilities)-1)
             new_node = Node(possibilities[idx], parent.children, None)
-            print("Switch", parent.type, "to", new_node.type)
+            print("Mutation (", parent.type, ", ", parent.value,
+                  ") -> (", new_node.type, ", ", new_node.value, ")")
             return new_node
 
     def mutate(self, root: Node) -> Node:
+        """
+        Performs point mutation on all nodes starting from root 
+        with probability of self.mutation_rate=10%. 
+        Returns the root of mutated tree
+        """
+
         queue = [root]
         while queue:
             node = queue.pop()
-            if random.random() < self.mutation_rate:
+            if random.random() < self.mutation_rate:  # 10%
                 new_node = self.perform_point_mutation(node)
                 node.type = new_node.type
                 node.value = new_node.value
@@ -102,7 +107,6 @@ class GP:
         return root
 
     def display_program(self, root: Node):
-        print("\n*** PROGRAM ***")
         level = 0
         stack = [(level, root)]
         while stack:
@@ -113,46 +117,49 @@ class GP:
                 stack.append((level + 1, child))
 
     def evolve(self, copy=False):
-        for generation in range(self.max_generations):
+        for generation in range(self.nr_of_generations):
+            print("\n\nGeneration", generation,
+                  " ----------------------------")
             if max(self.fitness)/len(self.population) > -0.1:
                 print("Solution found in generation", generation)
                 break
-            population_copy = self.population.copy()
-            fitness_copy = self.fitness.copy()
-            print("Generation", generation)
+
+            if copy:
+                population_copy = self.population.copy()
+                fitness_copy = self.fitness.copy()
 
             for i in range(self.population_size):
+                print("\nIndividual nr", i)
                 self.display_program(self.population[i])
                 print("Fitness", self.fitness[i])
-            print('\nOperations:')
+
+            print("\n***** Operations ***")
             for idx in range(len(self.population)):
-                if random.random() < self.crossover_rate:
+                if random.random() < self.crossover_rate:  # 50%
                     parent1 = self.perform_tournament()
                     parent2 = self.perform_tournament()
-                    child = self.perform_crossover(self.population[parent1], self.population[parent2])
+                    child = self.perform_crossover(
+                        self.population[parent1], self.population[parent2])
                     print("Crossover", parent1, parent2, end=' ')
                 else:
                     parent1 = self.perform_tournament()
                     child = self.mutate(self.population[parent1])
-                    print("Mutation", parent1, end=' ')
-                weakest = self.perform_negative_tournament()
+
+                weakest_idx = self.perform_negative_tournament()
                 if copy:
-                    population_copy[weakest] = child
-                    fitness_copy[weakest] = self.compute_fitness(child)
+                    population_copy[weakest_idx] = child
+                    fitness_copy[weakest_idx] = self.compute_fitness(child)
                 else:
-                    self.population[weakest] = child
-                    self.fitness[weakest] = self.compute_fitness(child)
-                print("->", weakest)
-        print("Best fitness:", max(self.fitness), "worst fitness:", min(self.fitness), "avg fitness:", sum(self.fitness)/len(self.fitness))
+                    self.population[weakest_idx] = child
+                    self.fitness[weakest_idx] = self.compute_fitness(child)
+                print("-> weakest idx:", weakest_idx)
+            print("\nBest fitness:", max(self.fitness), "worst fitness:", min(
+                self.fitness), "avg fitness:", sum(self.fitness)/len(self.fitness))
 
 
 if __name__ == "__main__":
-    print("---RUN---")
     gp = GP()
     gp.create_random_population()
-    # gp.display_program(gp.population[0])
-    # gp.display_program(gp.population[1])
-    # gp.population[0] = gp.mutate(gp.population[0])
-    # gp.display_program(gp.population[0])
     gp.evolve(copy=False)
+    print("\nBest individual:")
     gp.display_program(gp.population[gp.fitness.index(max(gp.fitness))])
