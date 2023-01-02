c28c59f7cf70480bc2e45a5423302b0542176a46
pawel <pawel.magnu@gmail.com>
2023-01-02 01:37:24 +0100

    added more fitnesses

--- fitness/fit_ex_12_diff.py
+++ fitness/fit_ex_12_diff.py
@@ -4,7 +4,7 @@ def fitness(received_outs, expected_outs):
         fitness += -(10e+9 * abs(len(received_outs) - 2))
         return int(fitness)
     try:
-        fitness += -abs(received_outs[0] + received_outs[1] - expected_outs[0])
+        fitness += -abs(received_outs[0] - received_outs[1] - expected_outs[0])
     except Exception:
         fitness += -10e+9
 
