a0b8d982ca8debf2159a6298fd9c0758c4b68217
pawel <pawel.magnu@gmail.com>
2023-01-14 17:49:44 +0100

    more fitnesses

--- fitness/fit_ex_13.py
+++ fitness/fit_ex_13.py
@@ -1,11 +1,15 @@
 def fitness(received_outs, expected_outs, nodes=0):
     fitness_ = 0
     if len(received_outs) == 0:
-        fitness_ -= 1000000
+        fitness_ -= 100000
         fitness_ -= nodes*5
         return max(fitness_, -1000000)
-    fitness_ -= (len(received_outs)-1)*20
+    fitness_ -= (len(received_outs)-1)*100
     if fitness_ > -40000:
-        fitness_ -= int(abs((1 - received_outs[0]/expected_outs[0])*1000))
+        if received_outs[0] == expected_outs[0]:
+            fitness_ += 0
+        else:
+            fitness_ -= 1000
+        # fitness_ -= int(abs((1 - received_outs[0]/expected_outs[0])*1000))
 
     return max(fitness_, -1000000)
---
+++ fitness/fit_gecco_27.py
@@ -0,0 +1,15 @@
+def fitness(received_outs, expected_outs, nodes=0):
+    fitness_ = 0
+    if len(received_outs) == 0:
+        fitness_ -= 100000
+        fitness_ -= nodes*5
+        return max(fitness_, -1000000)
+    fitness_ -= (len(received_outs)-1)*100
+    if fitness_ > -40000:
+        if received_outs[0] == expected_outs[0]:
+            fitness_ += 0
+        else:
+            fitness_ -= 5000
+        # fitness_ -= int(abs((1 - received_outs[0]/expected_outs[0])*1000))
+
+    return max(fitness_, -1000000)
