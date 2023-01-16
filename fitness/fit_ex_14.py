60ef3c2dd8bc817ee8277ead8c663b7d28cc1edf
pawel <pawel.magnu@gmail.com>
2023-01-16 21:52:06 +0100

    fix of 1.4

--- fitness/fit_ex_14.py
+++ fitness/fit_ex_14.py
@@ -6,6 +6,10 @@ def fitness(received_outs, expected_outs, nodes=0):
         return max(fitness_, -1000000)
     fitness_ -= (len(received_outs)-1)*100
     if fitness_ > -40000:
+        if expected_outs[0] == received_outs[0]:
+            return fitness_
+        elif expected_outs[0] == 0:
+            expected_outs[0] = 1
         fitness_ -= int(abs((1 - received_outs[0]/expected_outs[0])*1000))
 
     return max(fitness_, -1000000)
