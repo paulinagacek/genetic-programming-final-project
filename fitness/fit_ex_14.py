62417ad1b6010bbf2651ac9780fd0596497cebff
pawel <pawel.magnu@gmail.com>
2023-01-18 19:44:49 +0100

    example 14

--- fitness/fit_ex_14.py
+++ fitness/fit_ex_14.py
@@ -10,6 +10,6 @@ def fitness(received_outs, expected_outs, nodes=0):
             return fitness_
         elif expected_outs[0] == 0:
             expected_outs[0] = 1
-        fitness_ -= int(abs((1 - received_outs[0]/expected_outs[0])*1000))
+        fitness_ -= int((received_outs[0] - expected_outs[0])**2)*100
 
     return max(fitness_, -1000000)
